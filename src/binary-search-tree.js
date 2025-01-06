031c832acbd99c3cc2cd0caab73fad507657c64f
grec88 <Lihtinmann88@yandex.ru>
2025-01-07 01:18:20 +0300

    feat: root and min, max methods are implemented

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,16 +1,18 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
   add(/* data */) {
@@ -34,13 +36,29 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootNode) {
+      return null;
+    }
+
+    const current = this.rootNode;
+    while (current.left) {
+      current = current.left;
+    }
+
+    return current.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootNode) {
+      return null;
+    }
+
+    const current = this.rootNode;
+    while (current.right) {
+      current = current.right;
+    }
+
+    return current.data;
   }
 }
 
